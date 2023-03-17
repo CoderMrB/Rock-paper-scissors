9c6b5255f295986beb9ac4320c6f2b7d74c39b12
Christopher Webb <webbophone@gmail.com>
2023-03-17 12:11:06 +0000

    main.js started

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
-/* Plan
+let compWins = 0
+let userWins = 0
+
+let userInput = prompt(`Type Rock, Paper, or Scissors.`)
+console.log(userInput)
+
+if (userInput === `Rock` || `Paper` || `Scissors`) {
+    console.log(`User has entered valid response.`)
+}
+else {
+    console.log(`User has entered invalid response.`)
+}
+
+//The following What beats what?
+// If the user's input is set to paper and the computer's input is set to scissors, 
 
 
-*/
-
-
-
-
-console.log(`hello`)
+// compare user's input against the 
+/* let rules = [
+    [`rock`, `scissors`],
+    [`scissors`, `paper`],
+    [`paper`, `rock`]
+]*/
