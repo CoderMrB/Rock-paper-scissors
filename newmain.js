70b912a5db6e8658a7d185c82b80c446a752ce71
Christopher Webb <webbophone@gmail.com>
2023-03-17 14:24:19 +0000

    Task 3 done.

--- newmain.js
+++ newmain.js
@@ -1,5 +1,5 @@
-let playerMove = "rock";
-let computerMove = "rock";
+// let hardCodedPlayerMove = "rock";
+ let hardCodedComputerMove = "rock";
 
 /*
     com     player
@@ -10,24 +10,7 @@ let computerMove = "rock";
     S       P --
     S       R -- */
 
-/*if (playerMove === "rock" && computerMove === "paper"){
-    console.log("Player had rock, computer had paper. Computer wins")}
-    else if(playerMove === "paper" && computerMove === "scissors"){
-        console.log("Player had paper, computer had scissors. Computer wins")}
-    else if(playerMove === "scissors" && computerMove === "rock"){
-        console.log("Player had scissors, computer had rock. Computer wins")}
-    else if(playerMove === "scissors" && computerMove === "paper"){
-        console.log("Player had scissors, computer had paper. Player wins")}
-    else if(playerMove === "rock" && computerMove === "scissors"){
-        console.log("Player had rock, computer had scissors. Player wins")}
-    else if(playerMove === "paper" && computerMove === "rock"){
-        console.log("Player had paper, computer had rock. Player wins")}
-    else if(playerMove === computerMove){
-        console.log("Player and computer had the same thing. It's a draw!")
-    }*/
-
-
-    function getWinner(player, computer){
+    function getWinner(playerMove, computerMove){
         if (playerMove === "rock" && computerMove === "paper"){
     //console.log("Player had rock, computer had paper. Computer wins")
     return -1;
@@ -58,4 +41,4 @@ let computerMove = "rock";
     }
     }
 
-    console.log(getWinner())
+   alert(getWinner(prompt(`Type Rock, Paper, or Scissors.`), hardCodedComputerMove))
