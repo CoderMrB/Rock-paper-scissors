6b5f63514bde5c28713218458a246456d8e1ea93
Chris Branson <chrisbranson@live.co.uk>
2023-03-17 15:31:01 +0000

    Task 6 done

--- newmain.js
+++ newmain.js
@@ -1,6 +1,10 @@
 // let hardCodedPlayerMove = "rock";
 // let hardCodedComputerMove = "rock";
-    let possibleComputerMoves = [`rock`, `paper`, `scissors`]
+    let possibleComputerMoves = [`rock`, `paper`, `scissors`];
+    let numberOfGames = 0;
+    let playerWins = 0;
+    let playerLosses = 0;
+    let playerDraws = 0;
 /*
     com     player
     R       p --
@@ -13,30 +17,37 @@
     function getWinner(playerMove, computerMove){
         if (playerMove === "rock" && computerMove === "paper"){
     //console.log("Player had rock, computer had paper. Computer wins")
+    playerLosses ++;
     return -1;
 }
     else if(playerMove === "paper" && computerMove === "scissors"){
         //console.log("Player had paper, computer had scissors. Computer wins")
+        playerLosses ++;
         return -1;
     }
     else if(playerMove === "scissors" && computerMove === "rock"){
         //console.log("Player had scissors, computer had rock. Computer wins")
+        playerLosses ++;
         return -1;
     }
     else if(playerMove === "scissors" && computerMove === "paper"){
         //console.log("Player had scissors, computer had paper. Player wins")
+        playerWins ++;
         return 1;
     }
     else if(playerMove === "rock" && computerMove === "scissors"){
         //console.log("Player had rock, computer had scissors. Player wins")
+        playerWins ++;
         return 1;
     }
     else if(playerMove === "paper" && computerMove === "rock"){
         //console.log("Player had paper, computer had rock. Player wins")
+        playerWins ++;
         return 1;
     }
     else if(playerMove === computerMove){
         //console.log("Player and computer had the same thing. It's a draw!")
+        playerDraws ++;
         return 0;
     }
     }
@@ -48,8 +59,9 @@
     let wantsToKeepPlaying = true;
 
     while (wantsToKeepPlaying === true){
-    let randomInt = getRandomInt(3)
-    console.log(randomInt)
+    numberOfGames ++;
+    let randomInt = getRandomInt(3);
+    console.log(randomInt);
     
     let randomComputerMove = possibleComputerMoves[randomInt]
     console.log(`The computer chose ${randomComputerMove}`)
@@ -58,7 +70,9 @@ let userInput = prompt(`Type rock, paper, or scissors.`);
 console.log(`The player chose ${userInput}`);
    alert(getWinner(userInput, randomComputerMove));
 
-   if (confirm(`Would you like to play again?`) === false){
+   if (confirm(`You have played ${numberOfGames} game(s). You have ${playerWins} wins, ${playerLosses} losses, and ${playerDraws} draws. Would you like to play again?`) === false){
     wantsToKeepPlaying = false;
    }
     }
+
+/* There will be playerWins, playerLosses, playerDraws*/
